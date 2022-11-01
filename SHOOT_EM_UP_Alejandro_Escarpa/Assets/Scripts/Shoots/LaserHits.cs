f682439c855725966fc818c53bdd920f8fa1069d
TermiSenpai <escarpaprieto@gmail.com>
2022-11-01 11:47:13 +0100

    laser hit parent

--- SHOOT_EM_UP_Alejandro_Escarpa/Assets/Scripts/Shoots/LaserHits.cs
+++ SHOOT_EM_UP_Alejandro_Escarpa/Assets/Scripts/Shoots/LaserHits.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class LaserHits : MonoBehaviour
 {
     [SerializeField] private ParticleSystem hitFbx;
+    GameObject hitParent;
+    private void Start()
+    {
+        hitParent = GameObject.FindGameObjectWithTag("HitsParent");
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Instantiate(hitFbx, collision.transform);
+        Instantiate(hitFbx, collision.transform.position, Quaternion.identity, hitParent.transform);
 
         if (collision.collider.CompareTag("Enemy"))
         {
             Destroy(gameObject);
+            Destroy(collision.gameObject);
         }
     }
 }
